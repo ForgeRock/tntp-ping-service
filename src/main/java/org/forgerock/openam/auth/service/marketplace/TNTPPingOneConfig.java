87f245492190571ab66358452b9a781e9551f28b
Justin Chin <55926235+justinchinFR@users.noreply.github.com>
2024-02-22 19:50:44 +0000

    Add files via upload

---
+++ src/main/java/org/forgerock/openam/auth/service/marketplace/TNTPPingOneConfig.java
@@ -0,0 +1,37 @@
+package org.forgerock.openam.auth.service.marketplace;
+
+import org.forgerock.am.config.ServiceComponentConfig;
+import org.forgerock.openam.annotations.sm.Attribute;
+import org.forgerock.openam.annotations.sm.Id;
+
+public interface TNTPPingOneConfig extends ServiceComponentConfig{
+	
+	
+	@Id
+	String id();
+
+    @Attribute(order = 1)
+    default String environmentId(){ return ""; };    
+    
+    @Attribute(order = 200)
+    default String environmentRegion(){ return ""; };
+
+    @Attribute(order = 300)
+    default String p1APIKey(){ return ""; };
+
+    @Attribute(order = 400)
+    default String p1APISecret(){ return ""; };
+
+    @Attribute(order = 500)
+    default String p1RedirectURL(){ return ""; };
+    
+    @Attribute(order = 600)
+    default String dvAPIKey(){ return ""; };
+
+    @Attribute(order = 700)
+    default String clientIdWorkerApp(){ return ""; };
+
+    @Attribute(order = 800)
+    default String clientIdWorkerSecret(){ return ""; };
+	
+}
